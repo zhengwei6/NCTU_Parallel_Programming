1abf4b1c2cdcc803b9bb74f14ca9a26ac3f2654a
zhengwei6 <40619175+zhengwei6@users.noreply.github.com>
2019-12-06 00:13:51 +0800

    update

--- Final_Project/final_project.cu
+++ Final_Project/final_project.cu
@@ -21,10 +21,24 @@ __global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int K_W
 	C[Row*Col_Size + Col] = Pvalue;
 }
 
+__global__ void reluKernel(float *Input, float *Output) {
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	if (Input[index] < 0.02) {
+		Output[index] = 0.0;
+	}
+	else {
+		Output[index] = Input[index];
+	}
+}
+
 void matrixMultiplication(float *A, float *B, float *C, int Row_Size, int Col_Size, int K_Width) {
 	dim3 threadsPerBlock(32, 32);
 	dim3 blocksPerGrid(Row_Size / 32, Col_Size / 32);
 	matrixMultiplicationKernel << <blocksPerGrid, threadsPerBlock >> > (A, B, C, K_Width, Col_Size);
+}
+
+void relu(float *Input, float *Output, int size) {
+	reluKernel <<< size/32, 32>> > (Input,Output);
 }
 
 float* random_generate(float *M, size_t size) {
@@ -39,6 +53,7 @@ float* random_generate(float *M, size_t size) {
 
 int main(int argc, char *argv[]) {
 	// generate W1,W2,W3
+	/*
 	float *W1=NULL,*W2=NULL,*W3=NULL;
 	W1 = random_generate(W1, 64 * 32);
 	W2 = random_generate(W2, 32 * 64);
@@ -55,5 +70,21 @@ int main(int argc, char *argv[]) {
 		for (int j = 0;j < 64;j++)
 			printf("%f ", W3[i * 64 + j]);
 	}
+	*/
+	/*
+	float *W1 = NULL,*W3=NULL;
+	W1 = random_generate(W1, 128*1);
+	W3 = (float *)malloc(128 * 1 * sizeof(float));
+	dev_array W1_d(128, 1);
+	dev_array W3_d(128, 1);
+	W1_d.set(W1);
+	W3_d.set(W3);
+	relu(W1_d.getData(), W3_d.getData(), W3_d.getSize());
+	W3_d.get(W3);
+	for (int i = 0;i < 128;i++) {
+			printf("%f ", W3[i]);
+	}
+	*/
+
 
 }
